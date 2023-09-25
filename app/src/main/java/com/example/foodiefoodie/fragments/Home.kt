937ec2329c50f98134bfda2c671fbb506d1d380c
abhi2801 <abhishek.nayak560@gmail.com>
2023-09-25 15:10:43 +0530

    Image Slider Implemented1

--- app/src/main/java/com/example/foodiefoodie/fragments/Home.kt
+++ app/src/main/java/com/example/foodiefoodie/fragments/Home.kt
@@ -5,14 +5,51 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.denzcoskun.imageslider.ImageSlider
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.foodiefoodie.R
+import com.example.foodiefoodie.databinding.FragmentHomeBinding
 
 class Home : Fragment() {
+    lateinit var binding: FragmentHomeBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding= FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val imageList = ArrayList<SlideModel>() // Create image list
+
+// imageList.add(SlideModel("String Url" or R.drawable)
+// imageList.add(SlideModel("String Url" or R.drawable, "title") You can add title
+
+        imageList.add(SlideModel(R.drawable.burger, "The animal population decreased by 58 percent in 42 years."))
+        imageList.add(SlideModel(R.drawable.fish, "Elephants and tigers may become extinct."))
+        imageList.add(SlideModel(R.drawable.fries, "And people do that."))
+        imageList.add(SlideModel(R.drawable.idli, "The animal population decreased by 58 percent in 42 years."))
+        imageList.add(SlideModel(R.drawable.idli, "Elephants and tigers may become extinct."))
+        imageList.add(SlideModel(R.drawable.prawn, "And people do that."))
+        val imageSlider = binding.imageSlider
+        imageSlider.setImageList(imageList, ScaleTypes.FIT)
+
+        imageSlider.setItemClickListener(object :ItemClickListener{
+            override fun doubleClick(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(position: Int) {
+                val itemPos=imageList[position]
+                Toast.makeText(requireContext(), "$itemPos selected", Toast.LENGTH_SHORT).show()
+            }
+        })
+
     }
 }
